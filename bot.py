5a64ee482db4b405ce4ce2a33bcd867097ccab7b
Natalya <62794540+NatalyaKirsanova@users.noreply.github.com>
2025-11-07 13:24:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
+import os
+from telegram.ext import Application, CommandHandler
 
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+async def start(update, context):
+    await update.message.reply_text("✅ Бот работает!")
+
+app = Application.builder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()
